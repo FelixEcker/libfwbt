b489dd76f9016158634fa9a5ce507777010b35f3
Marie <marie@ha.si>
2024-05-13 18:50:14 +0200

    print serialized if enabled

--- src/test.c
+++ src/test.c
@@ -8,11 +8,30 @@ int main(void) {
   fwbt_t fwbt;
   fwbt_error_t err = fwbt_parse_bytes(test_fwbt, test_fwbt_len, &fwbt);
 
-  printf("Header Info:\n");
-  printf("\tSignature: %.*s ; Version: %u\n", 4, fwbt.header.signature,
+  if (err != FWBT_OK) {
+    goto exit;
+  }
+
+  fprintf(stderr, "Header Info:\n");
+  fprintf(stderr, "\tSignature: %.*s ; Version: %u\n", 4, fwbt.header.signature,
          fwbt.header.version);
-  printf("\tKey Width: %u ; Value Width: %u ; Entry Count: %u\n",
+  fprintf(stderr, "\tKey Width: %u ; Value Width: %u ; Entry Count: %u\n",
          fwbt.header.key_width, fwbt.header.value_width,
          fwbt.header.entry_count);
+
+  size_t outsize = 0;
+  uint8_t *out;
+
+  err = fwbt_serialize(fwbt, &out, &outsize);
+  if (err != FWBT_OK) {
+    goto exit;
+  }
+
+  fprintf(stderr, "outsize = %zu\n", outsize);
+#ifdef WRITE_SERIALIZED
+  fwrite(out, outsize, 1, stdout); 
+#endif
+
+exit:
   return err;
 }
