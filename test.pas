bbce5edb144013ce4f4e883a4864d97482b7aab0
marie <marie@ha.si>
2024-05-15 16:16:41 +0200

    test with modifications

--- test.pas
+++ test.pas
@@ -9,7 +9,7 @@ var
   wix: Integer;
 begin
   Assign(_file, path);
-  
+
   ReSet(_file);
   SetLength(LoadBytesFromFile, FileSize(_file));
 
@@ -43,10 +43,19 @@ begin
 end;
 
 var
+  test_array: array of array of UInt8;
+  ix: Integer;
+
   source_data, serialized_data: TByteDynArray;
   fwbt: TFWBT;
   err: TFWBTError;
 begin
+  SetLength(test_array, 4);
+  test_array[0] := [1, 3, 1, 2];
+  test_array[1] := [1, 2, 3, 4, 5, 6, 7, 8];
+  test_array[2] := [1, 3, 3, 7];
+  test_array[3] := [1, 2, 3, 4, 5, 6, 7, 8];
+
   source_data := LoadBytesFromFile('test.fwbt');
   if Length(source_data) = 0 then
     halt;
@@ -72,11 +81,57 @@ begin
     writeln('serialization error: ', err);
     halt;
   end;
-  
+
   writeln('Serialized Size: ', Length(serialized_data));
   if not CompareByteDynArray(source_data, serialized_data) then
   begin
     writeln('Serialized data does not match input data!');
     halt;
   end;
+
+  { Test Modification }
+  err := FwbtSetValue(fwbt, test_array[0], test_array[1], False);
+  if err <> FWBT_OK then
+  begin
+    writeln('setvalue 1 error: ', err);
+    halt;
+  end;
+
+  writeln('added value 1');
+  writeln('value 1 at index ', FwbtFindValue(fwbt, [1, 3, 1, 2]));
+
+  err := FwbtSetValue(fwbt, test_array[2], test_array[3], False);
+  if err <> FWBT_OK then
+  begin
+    writeln('setvalue 2 error: ', err);
+    halt;
+  end;
+
+  writeln('added value 2');
+  writeln('value 2 at index ', FwbtFindValue(fwbt, [1, 3, 3, 7]));
+
+  err := FwbtRemoveValue(fwbt, [1, 3, 1, 2]);
+  if err <> FWBT_OK then
+  begin
+    writeln('removevalue error: ', err);
+    halt;
+  end;
+
+  writeln('removed value 1');
+
+  err := FwbtSerialize(fwbt, serialized_data);
+  if err <> FWBT_OK then
+  begin
+    writeln('serialization error: ', err);
+    halt;
+  end;
+
+  writeln('Serialized Size: ', Length(serialized_data));
+  if CompareByteDynArray(source_data, serialized_data) then
+  begin
+    writeln('Serialized data matches input data!');
+    halt;
+  end;
+
+  writeln('modifications persist');
 end.
